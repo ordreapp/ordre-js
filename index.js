3207e4292cad957221149b072ca10a3d4cc2d660
newage99 <rooger99@gmail.com>
2023-08-15 21:12:19 +0200

    MutationObserver -> custom

--- index.js
+++ index.js
@@ -1,191 +1,282 @@
 let initialized = false;
+/*const config = { attributes: true, childList: true, subtree: true };
+const observingNodes = [];*/
 
-export function startOrdre() {
-    if (!("clientKey" in options) || !("deviceId" in options)) {
-        console.log("startOrdre: Missing 'clientKey' or 'deviceId' parameters.");
-        return;
-    }
-    const clientKey = options["clientKey"];
-    const deviceId = options["deviceId"];
+function nodeToDict (node) {
+    //const rect = node.getBoundingClientRect();
+    //console.log(rect);
+    return {
+        tagName: node.tagName
+    };
+};
+
+
+export function startOrdre(options) {
 
     if (initialized) {
         return;
     }
-
-    //console.log("initializeOrdre entered");
-
     initialized = true;
-    //let attributeName;
-    //let node;
-    let nodesToAddDict;
+
+    if (!("clientKey" in options) || !("ordreDeviceId" in options)) {
+        console.log("startOrdre: Missing 'clientKey' or 'ordreDeviceId' parameters.");
+        return;
+    }
+
+    let domIdNodesDict = {};
+    /*let nodesToAddDict;
     let nodesToAddKeys;
     let nodesToRemoveKeys;
     let nodeId;
     let observeCallbackNotTriggeredYet = true;
-    let domIdNodesDict = {};
-    let changed;
-    let callbackIsExecuting = false;
+    let changed;*/
 
-    const nodeToDict = node => {
-        //const rect = node.getBoundingClientRect();
-        //console.log(rect);
-        return {
-            tagName: node.tagName
-        };
+    const clientKey = options["clientKey"];
+    const deviceId = options["ordreDeviceId"];
+    const socket = new WebSocket("ws://192.168.1.53:8000/middleware/middleware_consumer/");
+
+    socket.onmessage = function(event) {
+        try {
+            console.log("ordre -> socket.onmessage entered");
+            const data = event.data;
+            const json = JSON.parse(data);
+            const type = json["type"];
+            if (type == "click_node") {
+                const nodeId = json["nodeId"];
+                console.log("ordre -> nodeId: " + nodeId);
+                if (nodeId in domIdNodesDict) {
+                    domIdNodesDict[nodeId].click();
+                } else {
+                    console.log("ordre -> Trying to click unexisting node (id=" + nodeId + ").");
+                }
+            }
+        } catch(e) {
+            console.log("ordre -> socket.onmessage exception: " + e.toString());
+        }
     };
 
-    function sleep(ms) {
+    function performAddNodesRequest(nodesToAdd) {
+
+        const nodesToSend = {};
+        nodesToAdd.forEach(node => {
+            nodesToSend[node.id] = nodeToDict(node);
+        });
+        const body = JSON.stringify({
+            deviceId,
+            nodes: nodesToSend
+        });
+    
+        fetch("http://192.168.1.53:8000/middleware/add_nodes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body
+        });
+    };
+    
+    function performRemoveNodesRequest(nodesToRemove) {
+    
+        const nodesToSend = nodesToRemove.map(node => node.id);
+        const body = JSON.stringify({
+            deviceId,
+            nodes: nodesToSend
+        });
+    
+        fetch("http://192.168.1.53:8000/middleware/remove_nodes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body
+        });
+    }
+
+    const analyzeIdElements = () => {
+        setTimeout(() => {
+            const idNodes = document.querySelectorAll('*[id]:not([id=""])');
+            const nodesToAdd = [];
+            let nodesToRemove = Object.keys(domIdNodesDict);
+            idNodes.forEach(node => {
+                const nodeId = node.id;
+                if (nodeId in domIdNodesDict) {
+                    nodesToRemove = nodesToRemove.filter(id => id !== nodeId);
+                } else {
+                    nodesToAdd.push(node);
+                }
+            });
+            if (nodesToAdd.length > 0) {
+                console.log(nodesToAdd, 'nodesToAdd');
+                performAddNodesRequest(nodesToAdd);
+                nodesToAdd.forEach(node => {
+                    const nodeId = node.id;
+                    if (!(nodeId in domIdNodesDict)) {
+                        //console.log('adding ' + nodeId + ' to domIdNodesDict');
+                        domIdNodesDict[nodeId] = node;
+                    }
+                });
+            }
+            if (nodesToRemove.length > 0) {
+                console.log(nodesToRemove, 'nodesToRemove');
+                performRemoveNodesRequest(nodesToRemove);
+                nodesToRemove.forEach(node => {
+                    const nodeId = node.id;
+                    if (nodeId in domIdNodesDict) {
+                        //console.log("removed key: " + key);
+                        delete domIdNodesDict[nodeId];
+                    }
+                });
+            }
+            analyzeIdElements();
+        }, 200);
+    };
+    analyzeIdElements();
+
+    /*function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    const callback = async (mutationList, observer) => {
-        try {
-            while (callbackIsExecuting) {
-                await sleep(1);
-            }
+    const processMutations = mutationList => {
 
-            nodesToAddDict = {};
-            //nodesToAdd = [];
-            //nodesToAddKeys = [];
-            nodesToRemoveKeys = [];
-
-            callbackIsExecuting = true;
-            changed = false;
-            if (observeCallbackNotTriggeredYet) {
-                const allElements = document.querySelectorAll('*[id]:not([id=""])');
-                if (allElements.length > 0) {
-                    console.log("allElements.length: " + allElements.length);
-                    allElements.forEach(node => {
-                        nodeId = node.id;
-                        console.log("nodeId: " + nodeId);
-                        nodesToAddDict[nodeId] = nodeToDict(node);
-                        //domIdNodesDict[nodeId] = nodeToDict(node);
-                        //domIdNodes.push(nodeToDict(node));
-                        //domIdNodesKeys.push(nodeId);
-                    });
-                    changed = true;
-                    //console.log(JSON.stringify(domIdNodes));
+        const addToObservingNodes = node => {
+            try {
+                if (observingNodes.includes(node)) {
+                    console.log(node, 'already exists');
+                } else {
+                    console.log(node, 'addToObservingNodes');
+                    new MutationObserver(callback).observe(node, config);
+                    observingNodes.push(node);
                 }
-                observeCallbackNotTriggeredYet = false;
+            } catch(e) {
+                console.log("ordre -> addToObservingNodes exception: " + e.toString());
             }
-
-            for (const mutation of mutationList) {
-                if (mutation.type === 'childList') {
-                    if (mutation.addedNodes.length > 0) {
-                        mutation.addedNodes.forEach(node => {
-                            nodeId = node.id;
-                            if (nodeId && nodeId.length > 0 && !(nodeId in nodesToAddDict)) {
-                                //console.log("added node.id: " + node.id);
-                                //nodesToAdd.push(nodeToDict(node));
-                                //nodesToAddKeys.push(nodeId);
-                                nodesToAddDict[nodeId] = nodeToDict(node);
-                            }
-                        });
-                    }
-                    if (mutation.removedNodes.length > 0) {
-                        mutation.removedNodes.forEach(node => {
-                            nodeId = node.id;
-                            if (nodeId && nodeId.length > 0 && !(nodesToRemoveKeys.includes(nodeId))) {
-                                //console.log("removed node.id: " + node.id);
-                                nodesToRemoveKeys.push(nodeId);
-                            }
-                        });
-                    }
-                }/* else if (mutation.type === 'attributes') {
-                    attributeName = mutation.attributeName;
-                    node = mutation.target;
-                    nodeId = node.id;
-                    if (attributeName && nodeId && nodeId.length > 0) {
-                        if (nodeId in domIdNodesDict) {
-                            domIdNodesDict[nodeId][attributeName] = node[attributeName];
-                            changed = true;
-                        }
-                    }
-                }*/
-            }
-
-            nodesToAddKeys = Object.keys(nodesToAddDict);
-            if (nodesToAddKeys.length > 0) {
-                //console.log("nodesToAddKeys: " + nodesToAddKeys.join(", "));
-
-                const body = JSON.stringify(nodesToAddDict);
-
-                fetch("http://192.168.1.533:8000/device/add_nodes", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body
-                });
-
-                nodesToAddKeys.forEach(key => {
-                    if (!(key in domIdNodesDict)) {
-                        //console.log("added key: " + key);
-                        //domIdNodesDict[key] = nodesToAdd[index];
-                        domIdNodesDict[key] = nodesToAddDict[key];
-                        changed = true;
-                    }
-                    /*if (!(domIdNodesKeys.includes(key))) {
-                        console.log("added key: " + key);
-                        domIdNodes.push(nodesToAdd[index]);
-                        //console.log("domIdNodesKeys before");
-                        //console.log(domIdNodesKeys);
-                        domIdNodesKeys.push(key);
-                        //console.log("domIdNodesKeys after");
-                        //console.log(domIdNodesKeys);
-                        changed = true;
-                    }*/
-                });
-            }
-            if (nodesToRemoveKeys.length > 0) {
-                //console.log("removedKeys: " + removedKeys.join(", "));
-
-                const body = JSON.stringify(nodesToRemoveKeys);
-
-                fetch("http://192.168.1.533:8000/device/remove_nodes", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body
-                });
-
-                nodesToRemoveKeys.forEach(key => {
-                    /*const pos = domIdNodesKeys.indexOf(key);
-                    if (pos != -1) {
-                        console.log("removed key: " + key);
-                        domIdNodes.splice(pos, 1);
-                        domIdNodesKeys.splice(pos, 1);
-                    }*/
-                    if (key in domIdNodesDict) {
-                        //console.log("removed key: " + key);
-                        delete domIdNodesDict[key];
-                        changed = true;
-                    }
-                });
-            }
-            if (changed) {
-                //console.log(".");
-                console.log("domIdNodesDict changed");
-                console.log(JSON.stringify(domIdNodesDict));
-            }
-        } catch (e) {
-            console.log("ordre MutationObserver exception: " + e.toString());
-            //console.log("ordre MutationObserver exception");
         }
-        callbackIsExecuting = false;
+
+        const recursivelyAddToObservingNodes = node => {
+            try {
+                addToObservingNodes(node);
+                for (let children of node.children) {
+                    //console.log([node, children], '[node, children]');
+                    recursivelyAddToObservingNodes(children);
+                }
+            } catch(e) {
+                console.log("ordre -> recursivelyAddToObservingNodes exception: " + e.toString());
+            }
+        };
+
+        nodesToAddDict = {};
+        //nodesToAdd = [];
+        //nodesToAddKeys = [];
+        nodesToRemoveKeys = [];
+        changed = false;
+
+        if (observeCallbackNotTriggeredYet) {
+            const allElementsWithId = document.querySelectorAll('*[id]:not([id=""])');
+            if (allElementsWithId.length > 0) {
+                //console.log("ordre -> allElementsWithId.length: " + allElementsWithId.length);
+                allElementsWithId.forEach(node => {
+                    nodeId = node.id;
+                    console.log("added node.id: " + node.id);
+                    //addToObservingNodes(node);
+                    nodesToAddDict[nodeId] = node;
+                    //domIdNodesDict[nodeId] = nodeToDict(node);
+                    //domIdNodes.push(nodeToDict(node));
+                    //domIdNodesKeys.push(nodeId);
+                });
+                changed = true;
+                //console.log(JSON.stringify(domIdNodes));
+            }
+            observeCallbackNotTriggeredYet = false;
+        }
+
+        for (const mutation of mutationList) {
+            if (mutation.type === 'childList') {
+                if (mutation.addedNodes.length > 0) {
+                    mutation.addedNodes.forEach(node => {
+                        //addToObservingNodes(node);
+                        recursivelyAddToObservingNodes(node);
+                        nodeId = node.id;
+                        if (nodeId && nodeId.length > 0 && !(nodeId in nodesToAddDict)) {
+                            console.log("added node.id: " + node.id);
+                            console.log(mutation.target, 'target');
+                            //nodesToAdd.push(nodeToDict(node));
+                            //nodesToAddKeys.push(nodeId);
+                            nodesToAddDict[nodeId] = node;
+                        }
+                    });
+                }
+                if (mutation.removedNodes.length > 0) {
+                    mutation.removedNodes.forEach(node => {
+                        nodeId = node.id;
+                        if (nodeId && nodeId.length > 0 && !(nodesToRemoveKeys.includes(nodeId))) {
+                            //console.log("removed node.id: " + node.id);
+                            nodesToRemoveKeys.push(nodeId);
+                        }
+                    });
+                }
+            }
+        }
+
+        nodesToAddKeys = Object.keys(nodesToAddDict);
+        if (nodesToAddKeys.length > 0) {
+            //console.log("nodesToAddKeys: " + nodesToAddKeys.join(", "));
+
+            sendNewNodes(nodesToAddDict);
+
+            nodesToAddKeys.forEach(key => {
+                if (!(key in domIdNodesDict)) {
+                    //console.log("added key: " + key);
+                    //domIdNodesDict[key] = nodesToAdd[index];
+                    domIdNodesDict[key] = nodesToAddDict[key];
+                    changed = true;
+                }
+            });
+        }
+        if (nodesToRemoveKeys.length > 0) {
+            //console.log("removedKeys: " + removedKeys.join(", "));
+
+            const body = JSON.stringify({
+                deviceId,
+                nodes: nodesToRemoveKeys
+            });
+
+            fetch("http://192.168.1.53:8000/middleware/remove_nodes", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body
+            });
+
+            nodesToRemoveKeys.forEach(key => {
+                if (key in domIdNodesDict) {
+                    //console.log("removed key: " + key);
+                    delete domIdNodesDict[key];
+                    changed = true;
+                }
+            });
+        }
+        if (changed) {
+            //console.log(".");
+            //console.log("ordre -> domIdNodesDict changed");
+            //console.log(JSON.stringify(domIdNodesDict));
+        }
     };
 
-    const observer = new MutationObserver(callback);
+    const callback = async (mutationList, observer) => {
+        try {
+            processMutations(mutationList);
+        } catch (e) {
+            console.log("ordre -> MutationObserver exception: " + e.toString());
+            //console.log("ordre MutationObserver exception");
+        }
+        //callbackIsExecuting = false;
+    };
 
-    // Options for the observer (which mutations to observe)
-    const config = { attributes: false, childList: true, subtree: true };
+    const bodyObserver = new MutationObserver(callback)
+    bodyObserver.observe(document.body, config);*/
 
-    console.log("initializeOrdre starting to observe...");
-
-    observer.observe(document, config);
-
-    let ordreObservationStartDetectionDiv;
+    /*let ordreObservationStartDetectionDiv;
 
     const observationStartDetector = () => {
         setTimeout(() => {
@@ -203,7 +294,7 @@ export function startOrdre() {
             }
         }, 10);
     };
-    observationStartDetector();
+    observationStartDetector();*/
 
     //setTimeout(() => observer.disconnect(), 10000);
 }
